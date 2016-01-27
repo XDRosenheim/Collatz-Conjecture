3ca68bd29f732a09a6a6b6bf14677c0e71c5099e
XDRosenheim <patrickrosenheim@gmail.com>
2016-01-27 08:00:35 +0100

    [1.0] It works now...

---
+++ someAlgorythm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace someAlgorythm {
+    class Program {
+        static void Main( string[] args ) {
+            while(true) {
+                long l;
+                while(true) {
+                    Console.WriteLine( "Collatz Conjecture" );
+                    Console.Write( "Number: " );
+                    l = Convert.ToInt64( Console.ReadLine() );
+                    if(l > 0) break;
+                }
+                var i = 0;
+                var x = l;
+                while(true) {
+                    Console.WriteLine( i + ": " + x );
+                    if(x == 1) break;
+                    if(x % 2 == 0) x = x / 2;
+                    else x = x * 3 + 1;
+                    i++;
+                }
+                Console.WriteLine();
+                Console.WriteLine( "Total runs: " + i );
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
